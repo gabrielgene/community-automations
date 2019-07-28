a4579ea7227e608e2e924987a47a70e1d707b39d
julianajennifer <julianajennifer99@gmail.com>
2019-07-28 01:35:45 -0300

    Add evolution logic

--- cypress/integration/automations/autor-iniciante.js
+++ cypress/integration/automations/autor-iniciante.js
@@ -1,24 +1,25 @@
-describe('Post Resource', function() {
+
+const users = ['julianajennifer', 'gabrielgene']; 
+
+describe('Beginner author', function() {
   beforeEach(() => {
     cy.auth();
   });
 
-  it('Creating a New Post', function() {
-    cy.visit('/'); // 1.
-
-    cy.get('input.post-title') // 2.
-      .type('My First Post'); // 3.
-
-    cy.get('input.post-body') // 4.
-      .type('Hello, world!'); // 5.
-
-    cy.contains('Submit') // 6.
-      .click(); // 7.
-
-    cy.url() // 8.
-      .should('include', '/posts/my-first-post');
-
-    cy.get('h1') // 9.
-      .should('contain', 'My First Post');
-  });
+  users.map(username => {
+    it(`Upgrade user: ${username}`, function() {
+      cy.visit('/');
+      cy.visit(`https://${username}.jusbrasil.com.br/gerenciar`);
+  
+      cy.get(':nth-child(9) > :nth-child(3) > :nth-child(1) > .btn').click();
+  
+      cy.get(':nth-child(5) > .selection-control > .selection-control-label').click();
+  
+      cy.get(':nth-child(13) > .selection-control > .selection-control-label').click();
+  
+      cy.get('.ProfileEdit-buttons-wrapper > .btn--green').click();
+    });
+  })
 });
+
+// visitar perfil pelo username 
